e44acf854c701a038fb9ebd1b75c13218bfcd5ae
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-10-26 17:19:51 +0300

    project created

---
+++ CarRentProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarRentProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
